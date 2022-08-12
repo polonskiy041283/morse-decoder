a17c58a06da66b47d3aa84e50540b6514aa26d07
polonskiy041283 <polonskiy041283@gmail.com>
2022-08-13 01:15:51 +0300

    task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    exprArr = expr.split('')
+    var numberOfSymbols = 0;
+    var symbolArr = [];
+    
+    for(var i = 0; i< exprArr.length; i+=10){
+      var str = ''
+        for(let y = 0; y<10; y++){
+          
+           str += expr[i+y];
+        }
+      symbolArr[numberOfSymbols] = str;
+      numberOfSymbols++;
+    }
+
+    for (let i = 0; i < symbolArr.length; i++) {
+        xArr = symbolArr[i].split('')
+        var morseArr = ''
+
+        for (let i = 0; i < 10; i++) {
+          xArr[0] == '0' ? xArr.splice(0,1) : null   
+        }
+
+        for (let y = 0; y < xArr.length; y+=2){
+          xArr[y+1] === '1' ? morseArr+= '-' : xArr[y+1] === '0'? morseArr+= '.' : morseArr = ' ';
+        }
+
+        for (var s in MORSE_TABLE){
+            if(s === morseArr){
+              morseArr = MORSE_TABLE[s]
+            }
+
+        symbolArr[i] = morseArr;
+      }
+    }
+    return symbolArr.join('')
 }
 
 module.exports = {
